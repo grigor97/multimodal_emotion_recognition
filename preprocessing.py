a5479375769446864b84cfa01d0e9877488f7c21
grigorkeropyan <goqorkeropyan@gmail.com>
2021-03-25 17:36:04 +0100

    added audio feature extractiong functions

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,83 @@
 import cv2
 import math
 import numpy as np
+import librosa
 from moviepy.editor import *
+
+
+def noise(data):
+    noise_amp = 0.035*np.random.uniform()*np.amax(data)
+    data = data + noise_amp*np.random.normal(size=data.shape[0])
+    return data
+
+
+def stretch(data, rate=0.8):
+    return librosa.effects.time_stretch(data, rate)
+
+
+def shift(data):
+    shift_range = int(np.random.uniform(low=-5, high=5)*1000)
+    return np.roll(data, shift_range)
+
+
+def pitch(data, sampling_rate, pitch_factor=0.7):
+    return librosa.effects.pitch_shift(data, sampling_rate, pitch_factor)
+
+
+def extract_features(data, sample_rate):
+    # ZCR
+    result = np.array([])
+    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+    result = np.hstack((result, zcr))  # stacking horizontally
+
+    # Chroma_stft
+    stft = np.abs(librosa.stft(data))
+    chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, chroma_stft))  # stacking horizontally
+
+    # MFCC
+    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mfcc))  # stacking horizontally
+
+    # Root Mean Square Value
+    rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
+    result = np.hstack((result, rms))  # stacking horizontally
+
+    # MelSpectrogram
+    mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mel))  # stacking horizontally
+
+    return result
+
+
+def get_features(path):
+    # duration and offset are used to take care of the no
+    # audio in start and the ending of each audio files as seen above.
+    # TODO fix this part of duration and offset
+    # data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
+    data, sample_rate = librosa.load(path)
+
+    # without augmentation
+    res1 = extract_features(data, sample_rate)
+    result = np.array(res1)
+
+    # data with noise
+    noise_data = noise(data)
+    res2 = extract_features(noise_data, sample_rate)
+    result = np.vstack((result, res2))  # stacking vertically
+
+    # data with stretching and pitching
+    new_data = stretch(data)
+    data_stretch_pitch = pitch(new_data, sample_rate)
+    res3 = extract_features(data_stretch_pitch, sample_rate)
+    result = np.vstack((result, res3))  # stacking vertically
+
+    # data with shift
+    shft_data = shift(data)
+    res4 = extract_features(shft_data, sample_rate)
+    result = np.vstack((result, res4))
+
+    return result
 
 
 def clip_video(video_path, start_time, end_time, save_path):
@@ -18,7 +94,6 @@ def clip_video(video_path, start_time, end_time, save_path):
         remove_temp=True)
 
     del video
-
     return save_path
 
 
@@ -32,6 +107,13 @@ def extract_video_images(vid_path, st, et, all_data_path):
         os.makedirs(save_folder)
 
     path_to_clip = clip_video(vid_path, st, et, save_folder + vid_n)
+
+    audio_clip = AudioFileClip(path_to_clip)
+    audio_path = save_folder + vid_name + '.wav'
+    audio_clip.write_audiofile(audio_path)
+
+    audio_features = get_features(audio_path)
+    np.save(save_folder + vid_name + '_features.npy', audio_features)
 
     cap = cv2.VideoCapture(path_to_clip)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
