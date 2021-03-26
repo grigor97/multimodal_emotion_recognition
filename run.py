02daa608fbf2a69c572a91d3db9edb8b52811a23
grigorkeropyan <goqorkeropyan@gmail.com>
2021-03-26 22:14:17 +0100

    enable run file to preprocess all the videos

--- run.py
+++ run.py
@@ -22,17 +22,17 @@ filtered_save_path = '/home/student/keropyan/data/preprocessed_data/filtered_emo
 # iemocap_divide_videos_to_clips(iemocap_data_path, preprocessed_data_path, iemocap_cliped_video_path)
 
 
-get_final_paths(preprocessed_data_path, filtered_save_path)
+# get_final_paths(preprocessed_data_path, filtered_save_path)
 
 
-# save_train_data_path = "/home/student/keropyan/data/preprocessed_data/train_data/"
-# save_test_data_path = "/home/student/keropyan/data/preprocessed_data/test_data/"
-#
-# train_paths = "/home/student/keropyan/data/preprocessed_data/filtered_emotions_paths/train_paths.csv"
-# test_paths = "/home/student/keropyan/data/preprocessed_data/filtered_emotions_paths/test_paths.csv"
-#
-# train_df_paths = pd.read_csv(train_paths)
-# test_df_paths = pd.read_csv(test_paths)
-#
-# prepare_whole_data(train_df_paths, save_train_data_path, 'final_train_paths.csv')
-# prepare_whole_data(test_df_paths, save_test_data_path, 'final_test_paths.csv')
+save_train_data_path = "/home/student/keropyan/data/preprocessed_data/train_data/"
+save_test_data_path = "/home/student/keropyan/data/preprocessed_data/test_data/"
+
+train_paths = "/home/student/keropyan/data/preprocessed_data/filtered_emotions_paths/train_paths.csv"
+test_paths = "/home/student/keropyan/data/preprocessed_data/filtered_emotions_paths/test_paths.csv"
+
+train_df_paths = pd.read_csv(train_paths)
+test_df_paths = pd.read_csv(test_paths)
+
+prepare_whole_data(train_df_paths, save_train_data_path, 'final_train_paths.csv')
+prepare_whole_data(test_df_paths, save_test_data_path, 'final_test_paths.csv')
