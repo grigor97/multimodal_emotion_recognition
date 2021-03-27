91389d6ada7ce24b8aa84090654e123aac583e7b
grigorkeropyan <goqorkeropyan@gmail.com>
2021-03-27 18:40:18 +0100

    corrected load path

--- lightgbm_audio.py
+++ lightgbm_audio.py
@@ -7,8 +7,8 @@ from sklearn.model_selection import KFold, cross_val_score
 
 final_emos = {'sad': 0, 'neu': 1, 'hap': 2, 'ang': 3, 'fru': 4, 'exc': 5, 'oth': 6}
 
-train_paths = "/home/student/keropyan/data/preprocessed_data/filtered_emotions_paths/train_paths.csv"
-test_paths = "/home/student/keropyan/data/preprocessed_data/filtered_emotions_paths/test_paths.csv"
+train_paths = "/home/student/keropyan/data/preprocessed_data/train_data/final_train_paths.csv"
+test_paths = "/home/student/keropyan/data/preprocessed_data/test_data/final_test_paths.csv"
 
 train_ps = pd.read_csv(train_paths)
 test_ps = pd.read_csv(test_paths)
