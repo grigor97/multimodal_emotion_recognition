5424cf7cd7f03bc8b0ce124c8e9fc30b7d4d44d6
grigorkeropyan <goqorkeropyan@gmail.com>
2021-03-27 11:25:00 +0100

    corrected save name

--- run3.py
+++ run3.py
@@ -3,7 +3,7 @@ from preprocessing import *
 save_train_data_path = "/home/student/keropyan/data/preprocessed_data/train_data/"
 save_name1 = 'final_train_paths1.csv'
 save_name2 = 'final_train_paths2.csv'
-save_name = 'final_train_paths2.csv'
+save_name = 'final_train_paths.csv'
 
 train_paths = "/home/student/keropyan/data/preprocessed_data/filtered_emotions_paths/train_paths.csv"
 
