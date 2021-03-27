7e124f286b9eb5b5cf157ecc4153f318d51c9609
grigorkeropyan <goqorkeropyan@gmail.com>
2021-03-27 19:06:48 +0100

    added code to save all ausio features

--- lightgbm_audio.py
+++ lightgbm_audio.py
@@ -36,23 +36,32 @@ def get_audio_features(df_paths):
 train_x, train_y = get_audio_features(train_ps)
 test_x, test_y = get_audio_features(test_ps)
 
+train_xp = "/home/student/keropyan/data/preprocessed_data/train_data/train_x.npy"
+train_yp = "/home/student/keropyan/data/preprocessed_data/train_data/train_y.npy"
+test_xp = "/home/student/keropyan/data/preprocessed_data/train_data/test_x.npy"
+test_yp = "/home/student/keropyan/data/preprocessed_data/train_data/test_y.npy"
+np.save(train_xp, train_x)
+np.save(train_yp, train_y)
+np.save(test_xp, test_x)
+np.save(test_xp, test_y)
+
 
 print("shape of train_x is {} and shape of train_y is {}".format(train_x.shape, train_y.shape))
 print("shape of test_x is {} and shape of test_y is {}".format(test_x.shape, test_y.shape))
 
-# This is my DL course project params:)
-clf = LGBMClassifier(boosting_type='gbdt', objective='multiclass',
-                     learning_rate=0.42352561266844885,
-                     max_depth=70,
-                     n_estimators=80)
-
-clf.fit(train_x, train_y)
-preds = clf.predict(train_y)
-acc = (train_y.reshape(1, -1) == preds.reshape(1, -1)).sum()/preds.size
-print("train accuracy is   ", acc)
-
-preds = clf.predict(test_x)
-acc = (test_y.reshape(1, -1) == preds.reshape(1, -1)).sum()/preds.size
-print("test accuracy is   ", acc)
-
-
+# # This is my DL course project params:)
+# clf = LGBMClassifier(boosting_type='gbdt', objective='multiclass',
+#                      learning_rate=0.42352561266844885,
+#                      max_depth=70,
+#                      n_estimators=80)
+#
+# clf.fit(train_x, train_y)
+# preds = clf.predict(train_y)
+# acc = (train_y.reshape(1, -1) == preds.reshape(1, -1)).sum()/preds.size
+# print("train accuracy is   ", acc)
+#
+# preds = clf.predict(test_x)
+# acc = (test_y.reshape(1, -1) == preds.reshape(1, -1)).sum()/preds.size
+# print("test accuracy is   ", acc)
+#
+#
