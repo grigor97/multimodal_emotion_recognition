e9c9cbaa48360ddf828900a14756a55a60ddad7b
grigorkeropyan <goqorkeropyan@gmail.com>
2021-03-29 15:28:42 +0200

    reduice epochs

--- audio_dl.py
+++ audio_dl.py
@@ -86,7 +86,7 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
 model_history = model.fit(np.expand_dims(train_x, -1),
                           labels_train_y,
                           batch_size=16,
-                          epochs=5,
+                          epochs=2,
                           validation_split=0.2,
                           callbacks=[cp_callback])
 
