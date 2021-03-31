b456cece25d29a455c9c1a7bc82568bbc8b85a73
grigorkeropyan <goqorkeropyan@gmail.com>
2021-03-31 09:44:18 +0200

    added restore modele

--- model_run.py
+++ model_run.py
@@ -1,15 +1,15 @@
 import argparse
 from utils.utils import load_cfg
-from utils.nn_utils import run_model
+from utils.nn_utils import *
 
 
 def parse_args():
-    parser = argparse.ArgumentParser("train model")
+    parser = argparse.ArgumentParser("train model, you can run following models: audio_cnn, audio_lstm, "
+                                     "audio_blstm, audio_stacked_lstm")
     parser.add_argument('-c', '--config', type=str, default='configs/config_paths.yml')
     parser.add_argument('-m', '--model', type=str)
-
-    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
-                        help='you can run following models: audio_cnn, audio_lstm, audio_blstm, audio_stacked_lstm')
+    parser.add_argument('-r', '--restore', type=str)
+    parser.add_argument('-cont', '--continue_at', type=int)
 
     return parser.parse_args()
 
@@ -18,8 +18,10 @@ def main(args):
     config = load_cfg(args.config)
     model_name = args.model
 
-    run_model(model_name, config)
-
+    if not args.restore:
+        run_model(model_name, config)
+    else:
+        cont_run_model(model_name, config, args.continue_at)
 
 
 if __name__ == '__main__':
