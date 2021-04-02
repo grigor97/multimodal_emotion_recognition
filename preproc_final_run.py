064f52444329f0a21f49d095eebd1bf57e3d89df
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-02 16:30:34 +0200

    check if folder exist

--- preproc_final_run.py
+++ preproc_final_run.py
@@ -2,15 +2,19 @@ from src.preprocessing import *
 from src.extracting_video_paths import *
 
 
-# TODO create folders if do not exist
 path_cfg = 'configs/config_paths.yml'
 cfg = load_cfg(path_cfg)
 logs_path = cfg['logs']['logs_path']
 preprocessed_data_path = cfg['data']['preprocessed_data_path']
+if not os.path.exists(preprocessed_data_path):
+    os.makedirs(preprocessed_data_path)
+
 iemocap_data_path = cfg['data']['iemocap_data_path']
+
 enterfece_data_path = cfg['data']['enterfece_data_path']
 ravdess_data_path = cfg['data']['ravdess_data_path']
 iemocap_cliped_video_path = cfg['data']['iemocap_cliped_video_path']
+
 filtered_save_path = cfg['data']['filtered_save_path']
 
 
