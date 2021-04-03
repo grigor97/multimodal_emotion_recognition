b0f6603993625a321bca85e48801adf63d3a0d9e
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-04 00:12:49 +0200

    correcting shapes

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -29,8 +29,8 @@ def run_video_model(model_name,
     test = load_pickle(test_pkl)
     # loading datasets
     audio_train = train['train_audio_data']
-    print('train pic shape is  {}'.format(np.asarray(train['train_pic_data']).shape))
-    pic_train = np.transpose(np.asarray(train['train_pic_data']), (0, 2, 3, 1))
+    print('train pic shape is  {}'.format(np.asarray(list(train['train_pic_data'])).shape))
+    pic_train = np.transpose(np.asarray(list(train['train_pic_data'])), (0, 2, 3, 1))
     labels_train = train['train_label_data']
 
     audio_test = test['test_audio_data']
