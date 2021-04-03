4c7d7b81c8913edb29d9d916c20285fbc81be46a
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-03 13:51:06 +0200

    trying to fix reader error

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -266,7 +266,7 @@ def iemocap_extract_video_images_and_audio_features(vid_path, st, et, all_data_p
     audio_save_path = save_folder + vid_name + '_' + str(nth_sub_video) + '.wav'
     path_to_clip = clip_video(vid_path, audio_save_path, st, et, save_folder + vid_name + '_' + str(nth_sub_video) + '.mp4')
 
-    path_to_audio = clip_audio(path_to_audio, st, et, audio_save_path)
+    path_to_audio = clip_audio(vid_path, st, et, audio_save_path)
 
     # audio_features = get_audio_features(path_to_audio)
     # audio_features_path = save_folder + vid_name + '_' + str(nth_sub_video) + '_features.npy'
