afaf96e5a9b4cf4ea52fe0549c6b9e60dc210a32
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-03 13:29:11 +0200

    trying to fix reader error

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -213,7 +213,7 @@ DISREGARD_LENGTH = 0.5  # if video length is less than DISREGARD_LENGTH then we 
 PIC_DIMS = (360, 240)
 
 
-def clip_video(video_path, start_time, end_time, save_path):
+def clip_video(video_path, audio_save_path, start_time, end_time, save_path):
     video = VideoFileClip(video_path)
     if end_time > video.duration:
         end_time = video.duration
@@ -225,6 +225,8 @@ def clip_video(video_path, start_time, end_time, save_path):
         audio_codec='aac',
         temp_audiofile='temp-audio.m4a',
         remove_temp=True)
+
+    sub_video.audio.write_videofile(audio_save_path)
 
     # del video.reader
     # del sub_video.reader
@@ -261,9 +263,10 @@ def iemocap_extract_video_images_and_audio_features(vid_path, st, et, all_data_p
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
 
-    path_to_clip = clip_video(vid_path, st, et, save_folder + vid_name + '_' + str(nth_sub_video) + '.mp4')
+    audio_save_path = save_folder + vid_name + '_' + str(nth_sub_video) + '.wav'
+    path_to_clip = clip_video(vid_path, audio_save_path, st, et, save_folder + vid_name + '_' + str(nth_sub_video) + '.mp4')
 
-    path_to_audio = clip_audio(path_to_audio, st, et, save_folder + vid_name + '_' + str(nth_sub_video) + '.wav')
+    # path_to_audio = clip_audio(path_to_audio, st, et, save_folder + vid_name + '_' + str(nth_sub_video) + '.wav')
 
     # audio_features = get_audio_features(path_to_audio)
     # audio_features_path = save_folder + vid_name + '_' + str(nth_sub_video) + '_features.npy'
@@ -327,14 +330,14 @@ def other_extract_video_images_and_audio_features(vid_path, st, et, all_data_pat
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
 
-    path_to_clip = clip_video(vid_path, st, et, save_folder + vid_name + '_' + str(nth_sub_video) + '.mp4')
-
-    audio_clip = AudioFileClip(path_to_clip)
     audio_path = save_folder + vid_name + '_' + str(nth_sub_video) + '.wav'
-    audio_clip.write_audiofile(audio_path)
+    path_to_clip = clip_video(vid_path, audio_path, st, et, save_folder + vid_name + '_' + str(nth_sub_video) + '.mp4')
+
+    # audio_clip = AudioFileClip(path_to_clip)
+    # audio_clip.write_audiofile(audio_path)
 
     # del audio_clip.reader
-    del audio_clip
+    # del audio_clip
 
     # audio_features = get_audio_features(audio_path)
     # audio_features_path = save_folder + vid_name + '_' + str(nth_sub_video) + '_features.npy'
