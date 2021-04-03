8645a0d0496f6fa18342ed060d0f033c66223e1e
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-03 15:49:09 +0200

    trying to fix reader error

--- src/extracting_video_paths.py
+++ src/extracting_video_paths.py
@@ -139,7 +139,7 @@ def iemocap_divide_videos_to_clips(data_path, pre_processed_data_path, video_sav
                     )
 
                 # TODO maybe remove
-                sub_video.audio.write_audiofile(video_name_folder + file_name + ".wav")
+                # sub_video.audio.write_audiofile(video_name_folder + file_name + ".wav")
 
                 # video.close()
                 # del video.reader
