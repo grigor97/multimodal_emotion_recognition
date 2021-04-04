1691a9c0a4eaea14c194375ba566679e406b1af9
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-04 19:03:32 +0200

    remove some pritns chgange shape

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -29,14 +29,12 @@ def run_video_model(model_name,
     test = load_pickle(test_pkl)
     # loading datasets
     audio_train = train['train_audio_data']
-    audio_train = np.array(audio_train.tolist())
-    print('train pic shape is  {}'.format(train['train_pic_data']))
+    audio_train = np.array(audio_train)
     pic_train = train['train_pic_data']
     labels_train = train['train_label_data']
 
     audio_test = test['test_audio_data']
-    audio_test = np.array(audio_test.tolist())
-    print('test pic shape is  {}'.format(test['test_pic_data']))
+    audio_test = np.array(audio_test)
     pic_test = test['test_pic_data']
     labels_test = test['test_label_data']
 
