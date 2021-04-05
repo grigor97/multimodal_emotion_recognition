7d00787afa6cbf5d5b631c1771b30710d5fee493
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-05 14:29:07 +0200

    save audio data and labels also

--- save_random_preprocessed_data.py
+++ save_random_preprocessed_data.py
@@ -28,18 +28,32 @@ def main(args):
     train_idx = np.random.randint(train_length, size=num)
     test_idx = np.random.randint(test_length, size=num)
 
+    train_samples_audio = audio_train[train_idx, :, :, 9]
+    train_samples_labels = labels_train[train_idx, :, :, 9]
     train_samples = pic_train[train_idx, :, :, 9]
-    test_samples = pic_test[test_idx, :, :, 9]
+
+    test_samples_audio = pic_test[test_idx, :, :, 9]
+    test_samples_labels = labels_test[test_idx, :, :, 9]
+    test_samples = audio_test[test_idx, :, :, 9]
+
+    samples_path = logs_path + 'samples/'
+    if not os.path.exists(samples_path):
+        os.makedirs(logs_path + 'samples/')
 
     for i in range(num):
         tr_im = Image.fromarray(train_samples[i])
         te_im = Image.fromarray(test_samples[i])
 
-        if not os.path.exists(logs_path + 'samples/'):
-            os.makedirs(logs_path + 'samples/')
+        tr_im.save(samples_path + str(i) + 'train.jpeg')
+        te_im.save(samples_path + str(i) + 'test.jpeg')
 
-        tr_im.save(logs_path + 'samples/' + str(i) + 'train.jpeg')
-        te_im.save(logs_path + 'samples/' + str(i) + 'test.jpeg')
+        with open(logs_path + str(i) + 'train_audio.txt', 'w') as f:
+            f.write("label is {}".format(train_samples_labels[i]))
+            f.write("audio data is \n {}".format(train_samples_audio[i]))
+
+        with open(logs_path + str(i) + 'test_audio.txt', 'w') as f:
+            f.write("label is {}".format(test_samples_labels[i]))
+            f.write("audio data is \n {}".format(test_samples_audio[i]))
 
 
 if __name__ == '__main__':
