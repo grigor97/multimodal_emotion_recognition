faeefae2e81d3784da344134a3941bf87e1a0c85
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-15 12:16:03 +0200

    chnaged stoping condition

--- utils/nn_utils.py
+++ utils/nn_utils.py
@@ -26,6 +26,9 @@ class CustomEarlyStopping(tf.keras.callbacks.Callback):
         t_acc = logs.get('accuracy')
 
         if t_acc - v_acc > self.tol:
+            if t_acc - v_acc > self.tol + 10:
+                print('gap is too larger wait time is {}'.format(self.wait))
+                self.wait += 100
             print('gap is larger wait time is {}'.format(self.wait))
             self.wait += 1
         else:
