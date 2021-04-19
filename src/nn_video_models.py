b62044fea080b0f5131725a776da2a7fa6d0c63f
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-19 15:45:22 +0200

    chnaged video cnn model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -200,14 +200,17 @@ def create_video_cnn_model(optimizer, audio_dim, pic_shape, output_dim):
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(16, kernel_size=(3, 3), padding="same")(pic_x)
+    # pic_x = Conv2D(64, kernel_size=(3, 3), padding="same")(pic_x)
+    # # pic_x = BatchNormalization()(pic_x)
+    # pic_x = Activation(activations.relu)(pic_x)
+    # pic_x = MaxPool2D()(pic_x)
+
+    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
     # pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
     pic_x = Flatten()(pic_x)
-    pic_x = Dense(64, activation='relu')(pic_x)
-    pic_x = Dropout(0.25)(pic_x)
     pic_x = Dense(32, activation='relu')(pic_x)
     # end of pictures network part
 
