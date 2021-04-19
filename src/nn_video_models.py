7e48fe61d8a53b23d8f884ffc0a8d407c95bf770
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-19 18:54:22 +0200

    changed testign model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -884,17 +884,17 @@ def create_video_testing_model(optimizer, audio_dim, pic_shape, output_dim, lstm
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(128, kernel_size=(3, 3), padding="same")(pic_x)
-    pic_x = BatchNormalization()(pic_x)
-    pic_x = Activation(activations.relu)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
+    # pic_x = Conv2D(128, kernel_size=(3, 3), padding="same")(pic_x)
+    # pic_x = BatchNormalization()(pic_x)
+    # pic_x = Activation(activations.relu)(pic_x)
+    # pic_x = MaxPool2D()(pic_x)
 
     pic_x = Conv2D(64, kernel_size=(3, 3), padding="same")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(64, kernel_size=(3, 3), padding="same")(pic_x)
+    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
