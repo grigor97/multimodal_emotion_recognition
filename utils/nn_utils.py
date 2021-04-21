69586d605a3b1655e79d7e271d1bae15003c0d21
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-21 09:24:17 +0200

    chnaged model

--- utils/nn_utils.py
+++ utils/nn_utils.py
@@ -10,7 +10,7 @@ from tensorflow.keras.utils import to_categorical
 
 
 class CustomEarlyStopping(tf.keras.callbacks.Callback):
-    def __init__(self, test_data, tol=0.17, patience=4):
+    def __init__(self, test_data, tol=0.12, patience=4):
         super(CustomEarlyStopping, self).__init__()
         self.tol = tol
         self.patience = patience
