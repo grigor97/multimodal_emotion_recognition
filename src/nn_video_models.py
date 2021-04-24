aa71a744723feded4400896f21ef936b98664f86
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-24 19:25:14 +0200

    changed model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -336,7 +336,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
 
     x = Dense(32, activation='relu')(x)
     x = Dropout(0.2)(x)
-    x = Dense(32, activation='relu')(x)
+    # x = Dense(32, activation='relu')(x)
     # TODO improve
     # BATCHNORM_CNN
     out = Dense(output_dim, activation='relu')(x)
