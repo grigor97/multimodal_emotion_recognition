23e474e16427399c38e3b55ebc00abc5a7375bd5
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-24 09:16:55 +0200

    changed model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -913,7 +913,7 @@ def create_video_testing_model(optimizer, audio_dim, pic_shape, output_dim, lstm
     # x = BatchNormalization()(x)
     # x = Activation(activations.relu)(x)
     x = Dense(32, activation='relu')(x)
-    x = Dropout(0.25)(x)
+    x = Dropout(0.15)(x)
     out = Dense(output_dim, activation='relu')(x)
 
     model = Model(
