e806eaba29111f1e6774083da121403c116105f6
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-24 18:34:31 +0200

    changed model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -295,15 +295,15 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
+    pic_x = Conv2D(32, strides=2, kernel_size=(3, 3), padding="same")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
-    pic_x = BatchNormalization()(pic_x)
-    pic_x = Activation(activations.relu)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
+    # pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
+    # pic_x = BatchNormalization()(pic_x)
+    # pic_x = Activation(activations.relu)(pic_x)
+    # pic_x = MaxPool2D()(pic_x)
 
     pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
     pic_x = BatchNormalization()(pic_x)
