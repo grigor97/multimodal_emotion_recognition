e1b0470b5b433d5c41672128e864d5ec456073d6
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-24 19:07:52 +0200

    changed model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -290,7 +290,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     # pictures network part
     pic_input = Input(shape=pic_shape, name='pic_input')
 
-    pic_x = Conv2D(16, strides=2, kernel_size=(3, 3), padding="same")(pic_input)
+    pic_x = Conv2D(16, kernel_size=(3, 3), padding="same")(pic_input)
 
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = BatchNormalization()(pic_x)
@@ -302,26 +302,27 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = BatchNormalization()(pic_x)
     # pic_x = Dropout(0.5)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
-
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
-    # pic_x = BatchNormalization()(pic_x)
-    pic_x = Activation(activations.relu)(pic_x)
-    pic_x = BatchNormalization()(pic_x)
-    # pic_x = Dropout(0.5)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
-
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
-    # pic_x = BatchNormalization()(pic_x)
-    pic_x = Activation(activations.relu)(pic_x)
-    pic_x = BatchNormalization()(pic_x)
-    # pic_x = Dropout(0.5)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
-
-    # pic_x = Conv2D(16, kernel_size=(3, 3), padding="same")(pic_x)
-    # pic_x = BatchNormalization()(pic_x)
-    # pic_x = Activation(activations.relu)(pic_x)
     # pic_x = MaxPool2D()(pic_x)
+
+    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
+    # pic_x = BatchNormalization()(pic_x)
+    pic_x = Activation(activations.relu)(pic_x)
+    pic_x = BatchNormalization()(pic_x)
+    # pic_x = Dropout(0.5)(pic_x)
+    pic_x = MaxPool2D()(pic_x)
+
+    pic_x = Conv2D(32, kernel_size=(3, 3), padding="same")(pic_x)
+    # pic_x = BatchNormalization()(pic_x)
+    pic_x = Activation(activations.relu)(pic_x)
+    pic_x = BatchNormalization()(pic_x)
+    # pic_x = Dropout(0.5)(pic_x)
+    pic_x = MaxPool2D()(pic_x)
+
+    pic_x = Conv2D(16, kernel_size=(3, 3), padding="same")(pic_x)
+    # pic_x = BatchNormalization()(pic_x)
+    pic_x = Activation(activations.relu)(pic_x)
+    pic_x = BatchNormalization()(pic_x)
+    pic_x = MaxPool2D()(pic_x)
 
     pic_x = Flatten()(pic_x)
     # pic_x = Dense(64, activation='relu')(pic_x)
