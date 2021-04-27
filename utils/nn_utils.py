corrected normalization

utils/nn_utils.py
@@ -141,14 +141,16 @@ def normalize_data(audio_train, audio_val, audio_test):
     :param audio_test: test data for audio
     :return: normalized data
     """
-    train_min = audio_train.min(axis=0).reshape((1, audio_train.shape[1]))
-    train_denom = audio_train.max(axis=0).reshape((1, audio_train.shape[1])) - audio_train.min(axis=0).reshape((1, audio_train.shape[1]))
+    train_min = audio_train.min(axis=0)
+    safe_max = np.abs(audio_train).max(axis=0)
+    safe_max[safe_max == 0] = 1
+
     audio_train -= train_min
     audio_val -= train_min
     audio_test -= train_min
-    audio_train /= train_denom.reshape((audio_train.shape[0], 1))
-    audio_val /= train_denom.reshape((audio_val.shape[0], 1))
-    audio_test /= train_denom.reshape((audio_test.shape[0], 1))
+    audio_train /= safe_max.reshape((audio_train.shape[0], 1))
+    audio_val /= safe_max.reshape((audio_val.shape[0], 1))
+    audio_test /= safe_max.reshape((audio_test.shape[0], 1))
 
     print("after nomilization  ")
     print(train_min)