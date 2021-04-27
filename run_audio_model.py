bacd6dd9b2cb3f42a5ddaf02799b9ea0ec178571
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-27 17:41:53 +0200

    changed run audio

--- run_audio_model.py
+++ run_audio_model.py
@@ -6,10 +6,10 @@ def parse_args():
     parser = argparse.ArgumentParser("train model, you can run following models: audio_cnn, audio_lstm, "
                                      "audio_blstm, audio_stacked_lstm")
     parser.add_argument('-c', '--config', type=str, default='configs/config_paths.yml')
-    parser.add_argument('-m', '--model', type=str)
+    parser.add_argument('-m', '--model', type=str, default='audio_cnn')
     parser.add_argument('-r', '--restore', type=bool)
     parser.add_argument('-cont', '--continue_at', type=int)
-    parser.add_argument('-opt', '--optimizer', type=str, default='RMSprop')
+    parser.add_argument('-opt', '--optimizer', type=str, default='Adam')
     parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
     parser.add_argument('-bs', '--batch_size', type=int, default=64)
     parser.add_argument('-iter', '--iterations', type=int, default=100)
