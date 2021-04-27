corrected normalization

utils/nn_utils.py
@@ -141,13 +141,14 @@ def normalize_data(audio_train, audio_val, audio_test):
     :param audio_test: test data for audio
     :return: normalized data
     """
-    train_min = audio_train.min(axis=0)
+    train_mean = audio_train.mean(axis=0)
+    audio_train -= train_mean
     safe_max = np.abs(audio_train).max(axis=0)
     safe_max[safe_max == 0] = 1
 
-    audio_train -= train_min
-    audio_val -= train_min
-    audio_test -= train_min
+    audio_val -= train_mean
+    audio_test -= train_mean
+
     audio_train /= safe_max
     audio_val /= safe_max
     audio_test /= safe_max