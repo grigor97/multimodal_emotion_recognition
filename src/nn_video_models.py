dacfa2079bdd53b7f6d47f74481cfd81be7052a5
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-28 16:04:58 +0200

    changed testing

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -193,27 +193,27 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     # pictures network part
     pic_input = Input(shape=pic_shape, name='pic_input')
 
-    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_input)
+    pic_x = Conv2D(16, kernel_size=(5, 5), padding="valid")(pic_input)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(32, kernel_size=(5, 5), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(32, kernel_size=(5, 5), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
+    # pic_x = MaxPool2D()(pic_x)
 
     # pic_x = Conv2D(64, kernel_size=(3, 3), padding="same")(pic_x)
     # # pic_x = BatchNormalization()(pic_x)
     # pic_x = Activation(activations.relu)(pic_x)
     # pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(16, kernel_size=(5, 5), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
@@ -222,7 +222,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     # pic_x = Dense(64, activation='relu')(pic_x)
     # pic_x = Dropout(0.25)(pic_x)
     pic_x = Dense(8, activation='relu')(pic_x)
-    # pic_x = Dropout(0.2)(pic_x)
+    pic_x = Dropout(0.2)(pic_x)
     # end of pictures network part
 
     # concatenation of two networks
@@ -922,36 +922,36 @@ def create_video_testing_model(optimizer, audio_dim, pic_shape, output_dim, lstm
     # pictures network part
     pic_input = Input(shape=pic_shape, name='pic_input')
 
-    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_input)
+    pic_x = Conv2D(16, kernel_size=(5, 5), padding="valid")(pic_input)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(32, kernel_size=(5, 5), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(32, kernel_size=(5, 5), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
+    # pic_x = MaxPool2D()(pic_x)
 
     # pic_x = Conv2D(64, kernel_size=(3, 3), padding="same")(pic_x)
     # # pic_x = BatchNormalization()(pic_x)
     # pic_x = Activation(activations.relu)(pic_x)
     # pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(16, kernel_size=(5, 5), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
-    # pic_x = MaxPool2D()(pic_x)
+    pic_x = MaxPool2D()(pic_x)
 
     pic_x = Flatten()(pic_x)
     # pic_x = Dense(64, activation='relu')(pic_x)
     # pic_x = Dropout(0.25)(pic_x)
     pic_x = Dense(8, activation='relu')(pic_x)
-    # pic_x = Dropout(0.5)(pic_x)
+    pic_x = Dropout(0.2)(pic_x)
     # end of pictures network part
 
     # concatenation of two networks
