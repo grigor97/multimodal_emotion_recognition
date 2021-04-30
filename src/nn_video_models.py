1b0cc5a5f970ad226607a8225e4d5395f08b8103
grigorkeropyan <goqorkeropyan@gmail.com>
2021-04-30 21:09:58 +0200

    corrected model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -239,7 +239,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     # x = Dropout(0.2)(x)
     # TODO improve
     # BATCHNORM_CNN
-    out = Dense(output_dim, activation='relu')(x)
+    out = Dense(output_dim, activation='softmax')(x)
 
     model = Model(
         inputs=[audio_input, pic_input],
