64fa5e2e6aea3f65e5d883854eb418fc3e01e717
grigorkeropyan <goqorkeropyan@gmail.com>
2021-05-02 18:03:49 +0200

    chaged model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -178,9 +178,9 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     audio_x = BatchNormalization()(audio_x)
     audio_x = Activation(activations.relu)(audio_x)
 
-    audio_x = Conv1D(64, 8, padding='same')(audio_x)
-    audio_x = BatchNormalization()(audio_x)
-    audio_x = Activation(activations.relu)(audio_x)
+    # audio_x = Conv1D(64, 8, padding='same')(audio_x)
+    # audio_x = BatchNormalization()(audio_x)
+    # audio_x = Activation(activations.relu)(audio_x)
 
     # audio_x = Conv1D(64, 8, padding='same')(audio_x)
     # audio_x = BatchNormalization()(audio_x)
@@ -234,7 +234,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     # pic_x = Dropout(0.3)(pic_x)
-    # pic_x = MaxPool2D()(pic_x)
+    pic_x = MaxPool2D()(pic_x)
 
     # pic_x = Conv2D(, kernel_size=(3, 3), padding="valid")(pic_x)
     # pic_x = BatchNormalization()(pic_x)
@@ -244,7 +244,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     # pic_x = Dense(64, activation='relu')(pic_x)
     # pic_x = Dropout(0.25)(pic_x)
     pic_x = Dense(8, activation='relu')(pic_x)
-    pic_x = Dropout(0.3)(pic_x)
+    pic_x = Dropout(0.7)(pic_x)
     # end of pictures network part
 
     # concatenation of two networks
