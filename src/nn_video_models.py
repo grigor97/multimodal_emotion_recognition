d8a09cf35d920cac435b07913821b6dbb696e431
grigorkeropyan <goqorkeropyan@gmail.com>
2021-05-06 17:21:36 +0200

    changed model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -167,9 +167,9 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     """
     # audio network part
     audio_input = Input(shape=(audio_dim, 1), name='audio_input')
-    audio_x = Conv1D(128, 8, padding='same', activation=activations.relu)(audio_input)
+    # audio_x = Conv1D(128, 8, padding='same', activation=activations.relu)(audio_input)
 
-    audio_x = Conv1D(128, 8, padding='same')(audio_x)
+    audio_x = Conv1D(128, 8, padding='same')(audio_input)
     audio_x = BatchNormalization()(audio_x)
     audio_x = Activation(activations.relu)(audio_x)
     # audio_x = Dropout(0.25)(audio_x)
@@ -202,7 +202,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     audio_x = Flatten()(audio_x)
     audio_x = Dense(32)(audio_x)
     audio_x = Activation(activations.relu)(audio_x)
-    # audio_x = Dropout(0.75)(audio_x)
+    audio_x = Dropout(0.5)(audio_x)
     # end of audio network part
 
     # pictures network part
