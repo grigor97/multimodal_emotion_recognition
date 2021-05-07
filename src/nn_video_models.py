3d08a6004d120e18ddc8abe1cb79f99b47283028
grigorkeropyan <goqorkeropyan@gmail.com>
2021-05-07 23:38:01 +0200

    chnaged model

--- src/nn_video_models.py
+++ src/nn_video_models.py
@@ -267,22 +267,26 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     # pic_x = Dropout(0.25)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     # pic_x = Dropout(0.25)(pic_x)
     pic_x = MaxPool2D()(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     # pic_x = Dropout(0.25)(pic_x)
 
-    pic_x = Conv2D(32, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_x)
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     # pic_x = Dropout(0.25)(pic_x)
     # pic_x = MaxPool2D()(pic_x)
+
+    pic_x = Conv2D(16, kernel_size=(3, 3), padding="valid")(pic_x)
+    pic_x = BatchNormalization()(pic_x)
+    pic_x = Activation(activations.relu)(pic_x)
 
     # pic_x = Conv2D(64, kernel_size=(3, 3), padding="same")(pic_x)
     # # pic_x = BatchNormalization()(pic_x)
@@ -293,7 +297,7 @@ def create_video_batchnorm_cnn_model(optimizer, audio_dim, pic_shape, output_dim
     pic_x = BatchNormalization()(pic_x)
     pic_x = Activation(activations.relu)(pic_x)
     # pic_x = Dropout(0.3)(pic_x)
-    pic_x = MaxPool2D()(pic_x)
+    # pic_x = MaxPool2D()(pic_x)
 
     # pic_x = Conv2D(, kernel_size=(3, 3), padding="valid")(pic_x)
     # pic_x = BatchNormalization()(pic_x)
